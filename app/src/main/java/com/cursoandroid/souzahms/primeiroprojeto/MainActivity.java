1fbe01190e82a95cb1947d92b5fc575cae5b1e42
henriquemalone <henrique.malone@gmail.com>
2018-08-03 13:15:17 -0300

    Initial commit

---
+++ app/src/main/java/com/cursoandroid/souzahms/primeiroprojeto/MainActivity.java
@@ -0,0 +1,53 @@
+package com.cursoandroid.souzahms.primeiroprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void recreate() {
+        super.recreate();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+}
